58a28c51db4581de5d8a6d4b7dca6eccca0dab22
Francerz <francerzam@hotmail.com>
2023-04-01 22:30:32 -0600

    FIX empty spaces

--- tests/AggregationsTest.php
+++ tests/AggregationsTest.php
@@ -16,20 +16,20 @@ class AggregationsTest extends TestCase
         $this->assertEquals([], Aggregations::mode($data));
         $this->assertEquals(8.6, Aggregations::mean($data));
         $this->assertEquals(13440, Aggregations::product($data));
- 
+
         $this->assertEquals(4, Aggregations::first($data));
         $this->assertEquals(2, Aggregations::last($data));
-        
+
         $this->assertEquals(2, Aggregations::min($data));
         $this->assertEquals(8, Aggregations::median($data));
         $this->assertEquals(15, Aggregations::max($data));
-        
+
         $this->assertEquals(2, Aggregations::percentile($data, 0));
         $this->assertEquals(4, Aggregations::percentile($data, 25));
         $this->assertEquals(8, Aggregations::percentile($data, 50));
         $this->assertEquals(14, Aggregations::percentile($data, 75));
         $this->assertEquals(15, Aggregations::percentile($data, 100));
-        
+
         $this->assertEquals('4, 8, 15, 14, 2', Aggregations::concat($data, ', '));
     }
 }

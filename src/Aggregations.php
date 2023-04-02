74cfd325745262f2e6b8af0c9802911c0e913520
Francerz <francerzam@hotmail.com>
2023-04-02 13:18:05 -0600

    ADD Aggregations::findPercentile mathod. ADD Aggregations functions DocComments. ADD $ignoreNulls or $ignoreEmpty in functions.

--- src/Aggregations.php
+++ src/Aggregations.php
@@ -4,107 +4,223 @@ namespace Francerz\PowerData;
 
 class Aggregations
 {
-    public static function count(array $values)
+    /**
+     * Counts items in array.
+     *
+     * @param array $values
+     * @param bool $ignoreNulls
+     * @return int
+     */
+    public static function count(array $values, bool $ignoreNulls = false)
     {
-        return count($values);
+        if (!$ignoreNulls) {
+            return count($values);
+        }
+        $count = 0;
+        foreach ($values as $v) {
+            if (isset($v)) {
+                $count++;
+            }
+        }
+        return $count;
     }
 
+    /**
+     * Sums every item in array.
+     *
+     * @param array $values
+     * @return int|float
+     */
     public static function sum(array $values)
     {
-        $sum = 0;
-        foreach ($values as $v) {
-            $sum += $v;
-        }
-        return $sum;
+        return array_sum($values);
     }
 
-    public static function product(array $values)
+    /**
+     * Multiplicates every value in array.
+     *
+     * @param array $values
+     * @param boolean $ignoreEmpty
+     * @return int|float
+     */
+    public static function product(array $values, bool $ignoreEmpty = false)
     {
         if (empty($values)) {
             return null;
         }
-        $product = array_shift($values);
-        foreach ($values as $v) {
-            $product *= $v;
+        if ($ignoreEmpty) {
+            return array_reduce($values, function($c, $v) {
+                if (empty($v)) {
+                    return $c;
+                }
+                return $c * $v;
+            }, 1);
         }
-        return $product;
+        return array_reduce($values, function ($c, $v) {
+            return $c * $v;
+        }, 1);
     }
 
+    /**
+     * Retrieves minimum value in array.
+     *
+     * @param array $values
+     * @return int|float
+     */
     public static function min(array $values)
     {
         if (empty($values)) {
             return null;
         }
-        $min = array_shift($values);
-        foreach ($values as $v) {
-            if ($min > $v) {
-                $min = $v;
-            }
-        }
-        return $min;
+        return min($values);
     }
 
+    /**
+     * Retrieves maximum value in array.
+     *
+     * @param array $values
+     * @return int|float
+     */
     public static function max(array $values)
     {
         if (empty($values)) {
             return null;
         }
-        $max = array_shift($values);
-        foreach ($values as $v) {
-            if ($max < $v) {
-                $max = $v;
-            }
-        }
-        return $max;
+        return max($values);
     }
 
+    /**
+     * Retrieves first value in array.
+     *
+     * @param array $values
+     * @return void
+     */
     public static function first(array $values)
     {
         return reset($values) ?: null;
     }
 
+    /**
+     * Retrieves last value in array.
+     *
+     * @param array $values
+     * @return mixed
+     */
     public static function last(array $values)
     {
         return end($values) ?: null;
     }
 
+    /**
+     * Concatenates every value in array.
+     *
+     * Alias of implode with inverted argruments.
+     *
+     * @param array $values
+     * @param string $separator
+     * @return string
+     */
     public static function concat(array $values, $separator = '')
     {
-        if (empty($values)) {
-            return '';
-        }
-        $concat = array_shift($values);
-        foreach ($values as $v) {
-            $concat .= $separator . (string)$v;
-        }
-        return $concat;
+        return implode($separator, $values);
     }
 
+    /**
+     * Counts all values in array.
+     *
+     * Alias of array_count_values.
+     *
+     * @param array $values
+     * @return array
+     */
+    public static function frequencies(array $values)
+    {
+        return array_count_values($values);
+    }
+
+    /**
+     * Filter the most frequents values in array.
+     *
+     * @param array $values
+     * @param boolean $strict
+     * @return array
+     */
     public static function mode(array $values, bool $strict = false)
     {
         if (empty($values)) {
             return [];
         }
-        $freq = [];
-        foreach ($values as $v) {
-            if (!isset($freq[$v])) {
-                $freq[$v] = 1;
-                continue;
-            }
-            $freq[$v] += 1;
-        }
+        $freq = self::frequencies($values);
         $modes = array_keys($freq, max($freq), $strict);
         return count($modes) == count($freq) ? [] : $modes;
     }
 
-    public static function mean(array $values)
+    /**
+     * Calculates the mean in given values array.
+     *
+     * @param array $values
+     * Array of values.
+     *
+     * @param bool $ignoreNulls
+     * Discards null values from calculation.
+     *
+     * @return float
+     */
+    public static function mean(array $values, bool $ignoreNulls = false)
     {
-        return static::sum($values) / (static::count($values) * 1.0);
+        return static::sum($values) / (static::count($values, $ignoreNulls) * 1.0);
     }
 
-    public static function percentile(array $values, float $percentile)
+    /**
+     * Retrieves median value in array.
+     *
+     * This is the same as retrieving 50th percentile.
+     *
+     * @param array $values
+     * Array of values.
+     *
+     * @return float
+     */
+    public static function median(array $values)
+    {
+        return static::percentile($values, 50);
+    }
+
+    /**
+     * Calculates percentile from first value appearance.
+     */
+    public const PERCENTILE_FLAGS_FIRST  = 0b00;
+    /**
+     * Calculates percentile from last value appearance.
+     */
+    public const PERCENTILE_FLAGS_LAST   = 0b01;
+    /**
+     * Remove duplicated values.
+     */
+    public const PERCENTILE_FLAGS_UNIQUE = 0b10;
+
+    /**
+     * Retrieves value at given percentile from a values collection.
+     *
+     * This function calculates fractional part from percentile if this doesn't match with any specific value.
+     *
+     * @param flaot[]|int[] $values
+     * Array with values to find.
+     *
+     * @param float $percentile
+     * Percentile from 0 to 100.
+     *
+     * @param int $flags
+     * Percentile flags. Only works with PERCENTILE_FLAGS_UNIQUE.
+     *
+     * @return float
+     */
+    public static function percentile(array $values, float $percentile, int $flags = self::PERCENTILE_FLAGS_FIRST)
     {
         $percentile = min(max(0, $percentile), 100);
+        if (($flags & self::PERCENTILE_FLAGS_UNIQUE)) {
+            $values = array_unique($values, SORT_ASC);
+        }
         sort($values);
         $values = array_values($values);
         $index = $percentile / 100 * (count($values) - 1);
@@ -118,8 +234,60 @@ class Aggregations
         return $val;
     }
 
-    public static function median(array $values)
+    /**
+     * Calculates percentile position for given value.
+     * 
+     * Percentile range goes from 0 to 100.
+     * 
+     * If $values array is empty, this function will return null.
+     * 
+     * If $value is lower than minimum then will return 0. If $value is higher to maximum then will return 100.
+     * 
+     * @param array $values
+     * Array of sample values.
+     *
+     * @param float $value
+     * Value to lookup.
+     *
+     * @param int $flags
+     * Determines the algorithm behavior, like removing duplicates and calculating from first or last appearance.
+     *
+     * @return float|null
+     */
+    public static function findPercentile(array $values, float $value, int $flags = self::PERCENTILE_FLAGS_FIRST)
     {
-        return static::percentile($values, 50);
+        if (empty($values)) {
+            return null;
+        }
+        if (($flags & self::PERCENTILE_FLAGS_UNIQUE)) {
+            $values = array_unique($values);
+        }
+        sort($values);
+        $values = array_values($values);
+        $firstI = $lastI = $nextI = 0;
+        $prevV = null;
+        foreach ($values as $k => $v) {
+            if ($v == $prevV) {
+                $lastI = $nextI = $k;
+                continue;
+            }
+            if ($v > $value) {
+                $nextI = $k;
+                break;
+            }
+            $firstI = $lastI = $k;
+            $prevV = $v;
+        }
+        $countLess1 = count($values) - 1;
+
+        $diff = $values[$nextI] - $values[$lastI];
+        $excess = $value - $values[$lastI];
+        $pct = ($flags & self::PERCENTILE_FLAGS_LAST) || $excess > 0 ?
+            $lastI / $countLess1 :
+            $firstI / $countLess1;
+        if ($value > $values[$lastI]) {
+            $pct += $diff > 0 ? $excess / ($diff * $countLess1) : 0;
+        }
+        return $pct * 100;
     }
 }
--- tests/AggregationsTest.php
+++ tests/AggregationsTest.php
@@ -25,11 +25,65 @@ class AggregationsTest extends TestCase
         $this->assertEquals(15, Aggregations::max($data));
 
         $this->assertEquals(2, Aggregations::percentile($data, 0));
+        $this->assertEquals(2.4, Aggregations::percentile($data, 5));
         $this->assertEquals(4, Aggregations::percentile($data, 25));
         $this->assertEquals(8, Aggregations::percentile($data, 50));
         $this->assertEquals(14, Aggregations::percentile($data, 75));
+        $this->assertEquals(14.8, Aggregations::percentile($data, 95));
         $this->assertEquals(15, Aggregations::percentile($data, 100));
 
+        $this->assertEquals(0, Aggregations::findPercentile($data, 2));
+        $this->assertEquals(5, Aggregations::findPercentile($data, 2.4));
+        $this->assertEquals(25, Aggregations::findPercentile($data, 4));
+        $this->assertEquals(50, Aggregations::findPercentile($data, 8));
+        $this->assertEquals(75, Aggregations::findPercentile($data, 14));
+        $this->assertEquals(95, Aggregations::findPercentile($data, 14.8));
+        $this->assertEquals(100, Aggregations::findPercentile($data, 15));
+
         $this->assertEquals('4, 8, 15, 14, 2', Aggregations::concat($data, ', '));
+    }
+
+    public function testPercentileFlags()
+    {
+        $data = [1, 1, 1, 2, 2, 2, 2, 4, 5, 5, 5, 8, 8];
+
+        $this->assertEquals(13, Aggregations::count($data));
+        $this->assertEquals(1, Aggregations::percentile($data, 0));
+        $this->assertEquals(8, Aggregations::percentile($data, 100));
+        $this->assertEquals(3, Aggregations::percentile($data, 54.16666666666));
+        $this->assertEquals(2, Aggregations::percentile($data, 50, Aggregations::PERCENTILE_FLAGS_FIRST));
+        // Higher has no difference in execution so its the same as Lower.
+        $this->assertEquals(2, Aggregations::percentile($data, 50, Aggregations::PERCENTILE_FLAGS_LAST));
+        // Unique: 1, 2, 4, 5, 8
+        $this->assertEquals(4, Aggregations::percentile($data, 50, Aggregations::PERCENTILE_FLAGS_UNIQUE));
+        $this->assertEquals(4, Aggregations::percentile(
+            $data,
+            50,
+            Aggregations::PERCENTILE_FLAGS_LAST | Aggregations::PERCENTILE_FLAGS_UNIQUE
+        ));
+
+        $this->assertEquals(00.0000000000, Aggregations::findPercentile($data, 0, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(00.0000000000, Aggregations::findPercentile($data, 1, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(25.0000000000, Aggregations::findPercentile($data, 2, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(54.1666666666, Aggregations::findPercentile($data, 3, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(58.3333333333, Aggregations::findPercentile($data, 4, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(66.6666666667, Aggregations::findPercentile($data, 5, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(86.1111111111, Aggregations::findPercentile($data, 6, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(88.8888888888, Aggregations::findPercentile($data, 7, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(91.6666666667, Aggregations::findPercentile($data, 8, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(100.000000000, Aggregations::findPercentile($data, 9, Aggregations::PERCENTILE_FLAGS_FIRST));
+        $this->assertEquals(100.000000000, Aggregations::findPercentile($data, 10, Aggregations::PERCENTILE_FLAGS_FIRST));
+
+        $this->assertEquals(00.0000000000, Aggregations::findPercentile($data, 0, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(16.6666666667, Aggregations::findPercentile($data, 1, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(50.0000000000, Aggregations::findPercentile($data, 2, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(54.1666666666, Aggregations::findPercentile($data, 3, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(58.3333333333, Aggregations::findPercentile($data, 4, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(83.3333333333, Aggregations::findPercentile($data, 5, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(86.1111111111, Aggregations::findPercentile($data, 6, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(88.8888888888, Aggregations::findPercentile($data, 7, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(100.000000000, Aggregations::findPercentile($data, 8, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(100.000000000, Aggregations::findPercentile($data, 9, Aggregations::PERCENTILE_FLAGS_LAST));
+        $this->assertEquals(100.000000000, Aggregations::findPercentile($data, 10, Aggregations::PERCENTILE_FLAGS_LAST));
     }
 }
